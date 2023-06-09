2d23589f86185c08c2eb2d1a168abf722f99514a
ST10091306 <ST10091306@vcconnect.edu.za>
2023-06-09 07:10:24 +0000

    added social club

--- DepartmentApp/Data/DataStore.cs
+++ DepartmentApp/Data/DataStore.cs
@@ -13,8 +13,8 @@ namespace DepartmentApp.Data
             new Department(2,"HR"),
             new Department(3,"FINANCE"),
             new Department(4,"LIBRARY"),
-            new Department(5,"CUSTOMER REALTIONS")
-
+            new Department(5,"CUSTOMER REALTIONS"),
+             new Department(6,"SOCIAL CLUB")
             };
     }
 }
